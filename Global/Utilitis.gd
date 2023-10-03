b8113fa990e4833e2a39fcaee5aba1a9ba448d90
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2023-10-02 22:21:19 -0600

    utiliti

--- Global/Utilitis.gd
+++ Global/Utilitis.gd
@@ -1,2 +1,16 @@
 extends Node
 
+const SAVE_PATH = "res://savegame.bin"
+
+func saveGame():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	var data: Dictionary = {
+		"playerHP": Game.playerHP,
+		"gold": Game.Gold
+	}
+	
+	var jstr = JSON.stringify(data)
+	file.store_string(jstr)
+	
+func  loadGame():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
