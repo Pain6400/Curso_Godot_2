e6177d5bce0456283637337331574f4ae0ee2e1e
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2023-10-03 21:41:15 -0600

    avance

--- main.gd
+++ main.gd
@@ -3,13 +3,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	Utilitis.saveGame()
+	Utilitis.loadGame()
 
 func _on_exit_pressed():
 	get_tree().quit()
